35023f497f57d9a93ab9bd31ce1f671741b32541
Gavin.Kimpson <gkimpson@gmail.com>
2017-05-29 14:22:53 +0100

    Lecture 45 : Product model factory and dummy images

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,6 +2,8 @@
 
 use App\User;
 use App\Category;
+use App\Product;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -33,5 +35,17 @@ $factory->define(Category::class, function (Faker\Generator $faker) {
     return [
     	'name' => $faker->word,
     	'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Poduct::class, function (Faker\Generator $faker) {
+   
+    return [
+    	'name' => $faker->word,
+    	'description' => $faker->paragraph(1),
+    	'quantity' => $faker->numberBetween(1, 10),
+    	'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+    	'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+    	'seller_id' => User::all()->random()->id,
     ];
 });
